c8717d2c06c45a17e74563f3ba5cc2dc8d699360
Flavia Dumitrica <flaviamihaela@yahoo.com>
2023-10-27 15:32:24 +0100

    first

--- fetch-service/src/main/java/com/programming/fetchservice/repository/FetchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programming.fetchservice.repository;
-
-import com.programming.fetchservice.model.Fetch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FetchRepository extends JpaRepository<Fetch, Long> {
-}
--- project-service/src/main/java/com/programming/projectservice/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programming.projectservice.repository;
-
-import com.programming.projectservice.model.Project;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProjectRepository extends MongoRepository<Project, String> {
-}
