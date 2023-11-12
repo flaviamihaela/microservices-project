2b22c50be61b61b8f101326ecf3f4996b5a77328
Flavia Dumitrica <flaviamihaela@yahoo.com>
2023-11-12 20:44:06 +0000

    Changes to be made: inv, fetch, tests, Grafana

--- project-service/src/main/java/com/programming/projectservice/ProjectServiceApplication.java
+++ project-service/src/main/java/com/programming/projectservice/ProjectServiceApplication.java
@@ -1,25 +1,37 @@
 // Define package for this class
 package com.programming.projectservice;
 
-import lombok.RequiredArgsConstructor;
+import com.programming.projectservice.model.Project;
+import com.programming.projectservice.repository.ProjectRepository;
 
 // Import necessary classes from the Spring Framework and Spring Boot
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-// Enable service to act as a client in a Eureka service discovery system
+// Annotation - marks class as Spring Boot application, excludes specific auto-configuration classes
 @SpringBootApplication
+// Enable service to act as a client in a Eureka service discovery system
 @EnableEurekaClient
 @RequiredArgsConstructor
-public class ProjectServiceApplication{
+public class ProjectServiceApplication implements CommandLineRunner {
 
-	//Main method - entry point of Spring Boot application
-	public static void main(String[] args) {
+    private final ProjectRepository projectRepository;
 
-		SpringApplication.run(ProjectServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServiceApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) {
+        if (projectRepository.count() < 1) {
+            Project project = new Project();
+            project.setName("iPhone 13");
+            project.setDescription("iPhone 13");
+
+            projectRepository.save(project);
+        }
+    }
 }
